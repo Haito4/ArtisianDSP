80e1b29912443ebfb63f01cab88b71ab13fd4bd5
Evan Tweedie <evan@Evans-MacBook-Pro.local>
2024-03-01 11:57:24 +1100

    Deleted Old Component CPP Files

--- Source/Impulse6Component.h
+++ Source/Impulse6Component.h
@@ -5,10 +5,19 @@
 class Impulse6Component : public juce::Component
 {
 public:
-    Impulse6Component();
-    ~Impulse6Component();
+    Impulse6Component()
+    {
+        impulseLabel.setFont(20.f);
+        impulseLabel.setJustificationType(juce::Justification::centred);
+        impulseLabel.setText("Impulse Response", juce::dontSendNotification);
+        addAndMakeVisible(impulseLabel);
+    }
+//    ~Impulse6Component();
     
-    void resized() override;
+    virtual void resized() override
+    {
+        impulseLabel.setBounds(getLocalBounds());
+    }
 private:
     juce::Label impulseLabel;
 };
